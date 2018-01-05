281796b301ca148149e660323cafb721eeee33dc
Alfonso Filho <alfonsocfilho@gmail.com>
2018-01-05 22:49:02 +0000

    style: interpreter indentation

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -3,34 +3,18 @@ import { FAIL, SUCCESS } from './constants';
 import { contains, is, isType, reverse } from './helpers';
 import { AstNode, AstType, MatchFail, MatchResult, MatchSucess, Pattern } from './types';
 
-const nodeReader: {[key: string]: any } = {
-
-  [AstType.LITERAL]: literal,
-
-  [AstType.OBJECT]: object,
-
-  [AstType.LIST]: list,
-
-  [AstType.BIND]: bind,
-
-  // [AstType.AS]: (input: any[], node: AstNode): MatchResult => {
-  //   const [ status, result ] = interpreter({root: node.value}, input);
-  //   return [ status, {...result, [node.name]: (Object as any ).values(result)} ];
-  // },
-
-  [AstType.REST]: rest,
-
+const astNodesMapping: {[key: string]: any } = {
+  [AstType.LITERAL]:  literal,
+  [AstType.OBJECT]:   object,
+  [AstType.LIST]:     list,
+  [AstType.BIND]:     bind,
+  [AstType.REST]:     rest,
   [AstType.WILDCARD]: wildcard,
-
-  [AstType.RANGE]: range,
-
+  [AstType.RANGE]:    range,
   [AstType.SEQUENCE]: sequence,
-
-  [AstType.OR]: or,
-
-  [AstType.AND]: and,
-
-  [AstType.REGEXP]: regexp
+  [AstType.OR]:       or,
+  [AstType.AND]:      and,
+  [AstType.REGEXP]:   regexp
 };
 
 export const interpreter = (pattern: Pattern, input: any[]): MatchResult => {
@@ -38,5 +22,5 @@ export const interpreter = (pattern: Pattern, input: any[]): MatchResult => {
   const { root } = pattern;
   const { type } = root;
 
-  return nodeReader[type](input, root);
+  return astNodesMapping[type](input, root);
 };
