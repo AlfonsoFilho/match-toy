d97c4003076c3cb6d2b023b2103d4f4be88c7348
Alfonso Filho <alfonsocfilho@gmail.com>
2019-01-03 19:10:05 -0200

    chore(examples): fixed runkit example

--- examples/runkit/index.js
+++ examples/runkit/index.js
@@ -1,16 +1,15 @@
-const { match } = require("match-toy");
+const { match } = require('match-toy');
 
 const factorial = match
   .case('0', () => 1)
-  .case('n', ({n}) => n * factorial(n - 1))
+  .case('n', ({ n }) => n * factorial(n - 1))
   .end();
 
-console.log('0', factorial(0))
-console.log('4', factorial(4))
-console.log('10', factorial(10))
+console.log('0', factorial(0));
+console.log('4', factorial(4));
+console.log('10', factorial(10));
 
-
-const howMany = match()
+const howMany = match
   .case('[]', () => 'empty')
   .case('[_]', () => 'one')
   .case('[_, _]', () => 'two')
@@ -18,8 +17,8 @@ const howMany = match()
   .else(() => 'it is not a list')
   .end();
 
-console.log(howMany([]))
-console.log(howMany([ 1 ]))
-console.log(howMany([ 1, 2 ]))
-console.log(howMany([ 1, 2, 3 ]))
-console.log(howMany('?'))
+console.log(howMany([]));
+console.log(howMany([1]));
+console.log(howMany([1, 2]));
+console.log(howMany([1, 2, 3]));
+console.log(howMany('?'));
